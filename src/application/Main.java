001b83fb9ee134c6fe204af0a0a4fbdf59740c34
krist7599555 <krist7599555@gmail.com>
2018-11-20 12:29:07 +0700

    add seats utils Str2Btn

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,30 +1,25 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+public class KTMain extends Application {
 
-public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			GridPane pane = new GridPane();
-			pane.add(new Map(), 0, 0);
-			pane.add(new Controll(), 1, 0);
-			Scene scene = new Scene(pane);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		GridPane root = new GridPane();
+		QuietRoom quietRoom = new QuietRoom();
+		root.add(quietRoom, 0, 0);
+		Scene scene = new Scene(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
 }
