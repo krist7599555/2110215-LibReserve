cfc09c278feb4fce72061632919b0b15b7325d16
krist7599555 <krist7599555@gmail.com>
2018-12-06 16:38:25 +0700

    update main

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,21 +14,13 @@ public class Main extends Application {
 		QuietRoom quietRoom = new QuietRoom();
 		root.add(quietRoom, 0, 0);
 		root.add(new Map(), 1, 0);
-<<<<<<< HEAD
-		root.add(new Controll(), 2, 0);
-//		root.add(new History(), 3, 0);
-=======
 		root.add(new ControlPane(), 2, 0);
->>>>>>> 996505be56431a20b3731f11004b313f65dd6e19
 		Scene scene = new Scene(root);
 
 		primaryStage.setScene(scene);
 		
 		primaryStage.show();
-<<<<<<< HEAD
-=======
 		
->>>>>>> 996505be56431a20b3731f11004b313f65dd6e19
 	}
 
 	public static void main(String[] args) {
